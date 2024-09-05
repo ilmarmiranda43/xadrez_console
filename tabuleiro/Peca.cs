5851419b738131f75c3ec018c24520919d491293
ilmarmiranda43 <ilmarmiranda43@gmail.com>
2024-09-05 15:40:23 -0300

    Primeiro commit - adicionando arquivos iniciais

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,53 @@
+﻿
+namespace tabuleiro
+{
+    internal abstract class Peca
+    {
+        public Posicao posicao {  get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab {  get; protected set; }
+
+        public Peca(Tabuleiro tab, Cor cor )
+        {
+            this.posicao = null;
+            this.cor = cor;
+            this.tab = tab;
+            this.qteMovimentos = 0;
+        }
+
+        public void incrementarQtdeMovimentos()
+        {
+            qteMovimentos++;
+        }
+
+        public void decrementarQtdeMovimentos()
+        {
+            qteMovimentos--;
+        }
+
+        public bool existeMOvimentoPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i=0; i<tab.linhas; i++)
+            {
+                for (int j=0; j<tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool movimentoPossivel(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
+
+        public abstract bool[,] movimentosPossiveis();
+        
+    }
+}
